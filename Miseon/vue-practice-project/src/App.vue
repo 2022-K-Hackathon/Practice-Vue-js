1806765da6b8d720315386a48e85ca8fb463b61c
Miseon <soup7912@gmail.com>
2022-07-12 17:11:40 +0900

    start

--- Miseon/vue-practice-project/src/App.vue
+++ Miseon/vue-practice-project/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h4>My First Web App</h4>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -21,6 +21,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+}
+
+.container {
+  background-color: palevioletred;
+  max-width: 600px;
+  height: 100vh;
+
+  /* center sort */
+  display: block; 
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
